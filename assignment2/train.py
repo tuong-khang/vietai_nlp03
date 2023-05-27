aeab41f1f4b19bd2793ba1f63803a82d8c6ab99b
Khang Duong <62482417+tuong-khang@users.noreply.github.com>
2023-05-28 00:24:31 +0700

    Add files via upload

--- assignment2/train.py
+++ assignment2/train.py
@@ -74,7 +74,7 @@ class Trainer:
         else:
             # TODO Otherwise, use 'torch.amp.autocast' context with the specified dtype, and initialize GradScaler if mixed_precision_dtype is float16.
             self.ctx = torch.amp.autocast(
-                device_type='cuda', dtype=mixed_precision_dtype)  # YOUR CODE HERE ###
+                device_type=f"cuda:{self.gpu_id}", dtype=mixed_precision_dtype)  # YOUR CODE HERE ###
             self.gradscaler = GradScaler()  # YOUR CODE HERE ###
 
     def _set_ddp_training(self):
@@ -209,6 +209,7 @@ class Trainer:
 
     def _eval(self, eval_dataloader, epoch: int):
         avg_loss = 0
+        self.model.to(f"cuda:{self.gpu_id}")
         self.model.eval()
         if _is_master_process():
             eval_progress_bar = tqdm(
